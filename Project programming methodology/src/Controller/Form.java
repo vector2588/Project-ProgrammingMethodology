09b5ac10ab5a85bd6307f787f7112dca3cce5257
vector2588 <59726607+vector2588@users.noreply.github.com>
2020-10-26 11:39:28 +0700

    ProjectProgrammingMethodology
    Backup file and do...

---
+++ Project programming methodology/src/Controller/Form.java
@@ -0,0 +1,69 @@
+package Controller;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Form {
+    Rectangle a;
+    Rectangle b;
+    Rectangle c;
+    Rectangle d;
+    Color color;
+    private String name;
+    public int form = 1;
+
+    public Form(Rectangle a, Rectangle b, Rectangle c, Rectangle d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    public Form(Rectangle a, Rectangle b, Rectangle c, Rectangle d, String name) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.name = name;
+
+        switch (name) {
+            case "j":
+                color = Color.DARKBLUE;
+                break;
+            case "l":
+                color = Color.ORANGE;
+                break;
+            case "o":
+                color = Color.GOLD;
+                break;
+            case "s":
+                color = Color.LIMEGREEN;
+                break;
+            case "t":
+                color = Color.PURPLE;
+                break;
+            case "z":
+                color = Color.RED;
+                break;
+            case "i":
+                color = Color.CYAN;
+                break;
+        }
+        this.a.setFill(color);
+        this.b.setFill(color);
+        this.c.setFill(color);
+        this.d.setFill(color);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void changeForm() {
+        if (form != 4) {
+            form++;
+        } else {
+            form = 1;
+        }
+    }
+}
